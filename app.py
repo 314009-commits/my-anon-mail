d481a33d448e785703d7383bbf66ad7c3bb32c13
314009-commits <314009@sssh.tyc.edu.tw>
2026-06-14 03:41:39 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -54,4 +54,7 @@ def send_email():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    # 優先讀取雲端分配的 Port，如果沒有（代表在自己電腦跑）就預設用 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
