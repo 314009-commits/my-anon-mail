185fdabdb6dcef26c155b9eb2ccc16d2f5c73df1
314009-commits <314009@sssh.tyc.edu.tw>
2026-06-14 23:50:03 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -14,28 +14,7 @@ def home():
 # 真正寄信功能
 @app.route("/send_email", methods=["POST"])
 def send_email():
-    try:
-        subject = request.form.get("subject", "").strip()
-        message = request.form.get("message", "").strip()
-
-        yag = yagmail.SMTP(
-            user=os.environ["EMAIL_USER"],
-            password=os.environ["EMAIL_PASSWORD"]
-        )
-
-        yag.send(
-            to=os.environ["RECEIVER_EMAIL"],
-            subject=f"[匿名信] {subject}",
-            contents=message
-        )
-
-        return "success"
-
-    except Exception as e:
-        import traceback
-        traceback.print_exc()   # 印完整錯誤到 Render Logs
-        return str(e), 500
-
+    return "API OK"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
