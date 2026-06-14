148ff0f1e299447326e8ca2a4913f1703f79f81a
314009-commits <314009@sssh.tyc.edu.tw>
2026-06-15 00:58:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,37 +1,12 @@
-from flask import Flask
-import os
-import smtplib
-from email.mime.text import MIMEText
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "SMTP 測試版"
+import socket
 
 @app.route("/send_email", methods=["POST"])
 def send_email():
+
     try:
+        socket.create_connection(("smtp.gmail.com", 587), timeout=10)
 
-        msg = MIMEText("Hello")
-        msg["Subject"] = "測試信"
-        msg["From"] = os.environ["EMAIL_USER"]
-        msg["To"] = os.environ["RECEIVER_EMAIL"]
-
-        server = smtplib.SMTP("smtp.gmail.com", 587, timeout=20)
-        server.starttls()
-
-        server.login(
-            os.environ["EMAIL_USER"],
-            os.environ["EMAIL_PASSWORD"]
-        )
-
-        server.send_message(msg)
-        server.quit()
-
-        return "SEND OK"
+        return "SMTP OK"
 
     except Exception as e:
-        import traceback
-        traceback.print_exc()
         return str(e), 500
