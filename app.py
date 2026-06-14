751747da6552b8dd6dc07608745782cbffdeb7d7
314009-commits <314009@sssh.tyc.edu.tw>
2026-06-14 20:52:05 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def send_email():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)port=port)
+    app.run(host="0.0.0.0", port=port)
