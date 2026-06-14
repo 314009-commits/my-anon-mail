6140beda606d53a8fa107cd008ee6a6d72447a0a
314009-commits <314009@sssh.tyc.edu.tw>
2026-06-15 00:11:37 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
-from flask import Flask, request
-from flask_cors import CORS
-import yagmail
+from flask import Flask
 import os
+import smtplib
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
-CORS(app)
 
-# 首頁測試用
 @app.route("/")
 def home():
-    return "匿名信箱後端運作中"
+    return "SMTP 測試版"
 
 @app.route("/send_email", methods=["POST"])
 def send_email():
     try:
-        yag.send(
-            to=os.environ["RECEIVER_EMAIL"],
-            subject="測試信",
-            contents="Hello"
+
+        msg = MIMEText("Hello")
+        msg["Subject"] = "測試信"
+        msg["From"] = os.environ["EMAIL_USER"]
+        msg["To"] = os.environ["RECEIVER_EMAIL"]
+
+        server = smtplib.SMTP("smtp.gmail.com", 587, timeout=20)
+        server.starttls()
+
+        server.login(
+            os.environ["EMAIL_USER"],
+            os.environ["EMAIL_PASSWORD"]
         )
+
+        server.send_message(msg)
+        server.quit()
 
         return "SEND OK"
 
@@ -26,7 +35,3 @@ def send_email():
         import traceback
         traceback.print_exc()
         return str(e), 500
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
